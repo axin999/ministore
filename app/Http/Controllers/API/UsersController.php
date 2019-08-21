af495fa3dcb48f4c6efc6e8cc15b1b3a3eb7fc10
axin <axin391@gmail.com>
2019-08-22 04:09:51 +0800

    add user profile component and file upload

--- app/Http/Controllers/API/UsersController.php
+++ app/Http/Controllers/API/UsersController.php
@@ -8,12 +8,48 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\UserFormRequest;
 use Illuminate\Support\Facades\Hash;
 use App\User;
+//use Intervention\Image\Facades\Image;
 
 class UsersController extends Controller
 {
+	public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
 	public function index(){
 		return User::latest()->paginate(10);
 	}
+
+    public function profile(){
+        return auth('api')->user();
+    }
+
+    public function updateProfile(Request $request){
+		$user = auth('api')->user();
+
+        function getExtension ($mime_type){
+                $extensions = array('image/jpeg' => 'jpg',
+                'text/xml' => 'xml'
+                );
+                // Add as many other Mime Types / File Extensions as you like
+                return $extensions[$mime_type];
+                }
+
+        if(isset($request->photo)){
+            $data = explode( ',', $request->photo );
+            $imgdata = base64_decode($data[1]);
+            $fifopen = finfo_open();
+            $photo_mime_type = finfo_buffer($fifopen, $imgdata, FILEINFO_MIME_TYPE);
+                
+            //$file_extension  = image_type_to_extension($photo_mime_type); 
+            $file_extension  = getExtension($photo_mime_type);
+            $save_filename = microtime() . '.' . $file_extension;
+            \Image::make($request->photo)->save(public_path('img/profile/').$save_filename);
+        }
+
+
+	}
+
     public function store(UserFormRequest $request){
 
 /*    	$this->validate($request,[
--- resources/assets/js/components/Profile.vue
+++ resources/assets/js/components/Profile.vue
@@ -1,23 +1,274 @@
-<template>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Profile Component</div>
+<style>
+    .widget-user-header{
+        background-position: center center;
+        background-size: cover;
+        background-size: 100% 100%;
 
-                    <div class="panel-body">
-                        I'm an Profile component!<router-link to="/item">url to example</router-link>
+       
+    }
+</style>
+
+<template>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="box box-widget widget-user">
+                <!-- Add the bg color to the header using any of the bg-* classes -->
+                <div class="widget-user-header bg-black" style="background-image:url('./img/profile-cover.jpg'); height: 250px;">
+                    <h3 class="widget-user-username">Elizabeth Pierce</h3>
+                    <h5 class="widget-user-desc">Web Designer</h5>
+                </div>
+                <div class="widget-user-image">
+                    <img class="img-circle" src="" alt="User Avatar">
+                </div>
+                <div class="box-footer">
+                    <div class="row">
+                        <div class="col-sm-4 border-right">
+                            <div class="description-block">
+                                <h5 class="description-header">3,200</h5>
+                                <span class="description-text">SALES</span>
+                            </div>
+                            <!-- /.description-block -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-sm-4 border-right">
+                            <div class="description-block">
+                                <h5 class="description-header">13,000</h5>
+                                <span class="description-text">FOLLOWERS</span>
+                            </div>
+                            <!-- /.description-block -->
+                        </div>
+                        <!-- /.col -->
+                        <div class="col-sm-4">
+                            <div class="description-block">
+                                <h5 class="description-header">35</h5>
+                                <span class="description-text">PRODUCTS</span>
+                            </div>
+                            <!-- /.description-block -->
+                        </div>
+                        <!-- /.col -->
                     </div>
+                    <!-- /.row -->
                 </div>
             </div>
         </div>
+
+
+        <!-- User Main Profile -->
+<div class="col-md-12">
+            <div class="card">
+              <div class="card-header p-2">
+                <ul class="nav nav-pills">
+
+                  <li class="nav-item"><a class="nav-link" href="#timeline" data-toggle="tab">Timeline</a></li>
+                  <li class="nav-item"><a class="nav-link active" href="#settings" data-toggle="tab">Settings</a></li>
+                </ul>
+              </div><!-- /.card-header -->
+              <div class="card-body">
+                <div class="tab-content">
+
+                  <!-- /.tab-pane -->
+                  <div class="tab-pane" id="timeline">
+                    <!-- The timeline -->
+                    <ul class="timeline timeline-inverse">
+                      <!-- timeline time label -->
+                      <li class="time-label">
+                        <span class="bg-danger">
+                          10 Feb. 2014
+                        </span>
+                      </li>
+                      <!-- /.timeline-label -->
+                      <!-- timeline item -->
+                      <li>
+                        <i class="fas fa-envelope bg-primary"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 12:05</span>
+
+                          <h3 class="timeline-header"><a href="#">Support Team</a> sent you an email</h3>
+
+                          <div class="timeline-body">
+                            Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
+                            weebly ning heekya handango imeem plugg dopplr jibjab, movity
+                            jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
+                            quora plaxo ideeli hulu weebly balihoo...
+                          </div>
+                          <div class="timeline-footer">
+                            <a href="#" class="btn btn-primary btn-sm">Read more</a>
+                            <a href="#" class="btn btn-danger btn-sm">Delete</a>
+                          </div>
+                        </div>
+                      </li>
+                      <!-- END timeline item -->
+                      <!-- timeline item -->
+                      <li>
+                        <i class="fas fa-user bg-info"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 5 mins ago</span>
+
+                          <h3 class="timeline-header border-0"><a href="#">Sarah Young</a> accepted your friend request
+                          </h3>
+                        </div>
+                      </li>
+                      <!-- END timeline item -->
+                      <!-- timeline item -->
+                      <li>
+                        <i class="fas fa-comments bg-warning"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 27 mins ago</span>
+
+                          <h3 class="timeline-header"><a href="#">Jay White</a> commented on your post</h3>
+
+                          <div class="timeline-body">
+                            Take me to your leader!
+                            Switzerland is small and neutral!
+                            We are more like Germany, ambitious and misunderstood!
+                          </div>
+                          <div class="timeline-footer">
+                            <a href="#" class="btn btn-warning btn-flat btn-sm">View comment</a>
+                          </div>
+                        </div>
+                      </li>
+                      <!-- END timeline item -->
+                      <!-- timeline time label -->
+                      <li class="time-label">
+                        <span class="bg-success">
+                          3 Jan. 2014
+                        </span>
+                      </li>
+                      <!-- /.timeline-label -->
+                      <!-- timeline item -->
+                      <li>
+                        <i class="fas fa-camera bg-purple"></i>
+
+                        <div class="timeline-item">
+                          <span class="time"><i class="far fa-clock"></i> 2 days ago</span>
+
+                          <h3 class="timeline-header"><a href="#">Mina Lee</a> uploaded new photos</h3>
+
+                          <div class="timeline-body">
+                            <img src="">
+                            <img src="">
+                            <img src="">
+                            <img src="">
+                          </div>
+                        </div>
+                      </li>
+                      <!-- END timeline item -->
+                      <li>
+                        <i class="far fa-clock bg-gray"></i>
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- /.tab-pane -->
+
+                  <div class="tab-pane active" id="settings">
+                    <form class="form-horizontal">
+                      <div class="form-group">
+                        <label for="inputName" class="col-sm-2 control-label">Name</label>
+
+                        <div class="col-sm-10">
+                          <input type="text" v-model="form.name" class="form-control" id="inputName" placeholder="Name">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+
+                        <div class="col-sm-10">
+                          <input type="email" v-model="form.email" class="form-control" id="inputEmail" placeholder="Email">
+                        </div>
+                      </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label" for="inputFilePhoto">Profile Photo</label>
+                        <div class="col-sm-10">
+                        <div class="input-group">
+                            <div class="custom-file">
+                                <input type="file" @change="updateProfile" class="custom-file-input" id="inputFilePhoto">
+                                <label class="custom-file-label" for="inputFilePhoto">Choose file</label>
+                            </div>
+                            <div class="input-group-append">
+                                <span class="input-group-text" id="">Upload</span>
+                            </div>
+                        </div>
+                       </div>
+                    </div>
+                      <div class="form-group">
+                        <label for="inputExperience" class="col-sm-2 control-label">Experience</label>
+
+                        <div class="col-sm-10">
+                          <textarea class="form-control" id="inputExperience" placeholder="Experience"></textarea>
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="inputPassport" class="col-sm-2 control-label">Passport</label>
+
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="inputPassport" placeholder="Passport">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" @click.prevent="updadateInfo" class="btn btn-success">Submit</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                  <!-- /.tab-pane -->
+                </div>
+                <!-- /.tab-content -->
+              </div><!-- /.card-body -->
+            </div>
+            <!-- /.nav-tabs-custom -->
+          </div>
+        <!-- end of user main profile -->
     </div>
+</div>
 </template>
 
 <script>
     export default {
+        data(){
+            return{
+                form: new Form({
+                    id:'',
+                    name:'',
+                    email:'',
+                    password:'',
+                    type:'',
+                    bio:'',
+                    photo:''
+                })
+            }       
+        },
         mounted() {
             console.log('Component mounted.')
+        },
+        methods:{
+            updadateInfo(){
+                this.form.put('api/profile')
+                .then( ()=>{
+
+                })
+                .cath( () =>{
+
+                });
+            },
+            updateProfile(pic){
+                let file = pic.target.files[0];
+                let reader = new FileReader();
+                reader.onloadend = (file) => {
+                    this.form.photo = reader.result;
+                    console.log('RESULT',reader.result);
+                }
+                reader.readAsDataURL(file);
+            }
+        },
+        created(){
+            Axios.get("api/profile").
+            then(({data}) => (this.form.fill(data)))
+
         }
     }
 </script>
