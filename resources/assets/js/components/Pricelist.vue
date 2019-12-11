8d684684944745c8ff5ee7ca3be5155fabe2b476
axin <axin391@gmail.com>
2019-12-11 23:13:51 +0800

    checking update

--- resources/assets/js/components/Pricelist.vue
+++ resources/assets/js/components/Pricelist.vue
@@ -5,7 +5,7 @@
 <div class="col-12">
             <div class="card">
               <div class="card-header">
-                <h3 class="card-title">Price List</h3>
+                <h3 class="card-title">Price Lists</h3>
 
                 <div class="card-tools">
                   <div class="input-group input-group-sm" style="width: 150px;">
